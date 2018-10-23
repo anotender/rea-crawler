d1b777017e5b75a29d0ca570fc01a67ae0759912
Mateusz Nowak <mateusznowak@vp.pl>
2018-10-23 18:53:44 +0200

    Added scheduler configuration prototype

--- src/main/kotlin/pl/edu/agh/rea/crawler/configuration/registry/CrawlerRegistry.kt
+++
@@ -1,5 +0,0 @@
-package pl.edu.agh.rea.crawler.configuration.registry
-
-interface CrawlerRegistry {
-
-}
---
+++ src/main/kotlin/pl/edu/agh/rea/crawler/configuration/scheduler/SchedulerConfiguration.kt
@@ -0,0 +1,33 @@
+package pl.edu.agh.rea.crawler.configuration.scheduler
+
+import kotlinx.coroutines.experimental.runBlocking
+import org.springframework.context.annotation.Configuration
+import pl.edu.agh.rea.crawler.configuration.properties.CrawlerConfigurationProperties
+import pl.edu.agh.rea.crawler.domain.Crawler
+import pl.edu.agh.rea.crawler.domain.VendorOffersCrawler
+import pl.edu.agh.rea.crawler.domain.model.Offer
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+import javax.annotation.PostConstruct
+
+@Configuration
+class SchedulerConfiguration(private val crawlerConfigurationProperties: CrawlerConfigurationProperties) {
+
+    private val threadPool = Executors.newScheduledThreadPool(crawlerConfigurationProperties.vendors.size)
+
+    @PostConstruct
+    private fun scheduleTasks() {
+        crawlerConfigurationProperties
+                .vendors
+                .map { VendorOffersCrawler(it) }
+                .forEach(this::scheduleCrawler)
+    }
+
+    private fun scheduleCrawler(crawler: Crawler<List<Offer>>) {
+        threadPool.schedule({
+            val offers = runBlocking { crawler.fetch() }
+            println(offers)
+        }, 4, TimeUnit.HOURS)
+    }
+
+}
