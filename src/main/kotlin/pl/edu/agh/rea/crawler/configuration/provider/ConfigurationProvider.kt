adbbbe440d419f93eea553e30b78c9290d76d120
Mateusz Nowak <mateusznowak@vp.pl>
2019-03-05 17:52:51 +0100

    Moved configuration back to main crawler project

--- src/main/kotlin/pl/edu/agh/rea/crawler/configuration/provider/ConfigurationProvider.kt
+++ src/main/kotlin/pl/edu/agh/rea/crawler/configuration/provider/ConfigurationProvider.kt
@@ -6,13 +6,12 @@ import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import pl.edu.agh.rea.crawler.configuration.properties.VendorConfigurationProperties
-import java.io.File
-import java.io.FileNotFoundException
+import java.net.URL
 import javax.annotation.PostConstruct
 import kotlin.system.exitProcess
 
 @Component
-class ConfigurationProvider(@Value("\${configurationFilePath}") private val configurationFilePath: String) {
+class ConfigurationProvider(@Value("\${vendor}") private val vendor: String) {
 
     companion object {
         private val LOGGER = LoggerFactory.getLogger(ConfigurationProvider::class.java)
@@ -22,14 +21,21 @@ class ConfigurationProvider(@Value("\${configurationFilePath}") private val conf
 
     @PostConstruct
     private fun readVendorConfigurationProperties() {
-        try {
-            LOGGER.info("Reading configuration from $configurationFilePath")
-            vendorConfigurationProperties = jacksonObjectMapper().readValue(File(configurationFilePath))
-            LOGGER.info("The config is: $vendorConfigurationProperties")
-        } catch (e: FileNotFoundException) {
-            LOGGER.error("The configuration file was not found. Please add --configurationFilePath argument or check if provided file exists.")
-            LOGGER.error("The app will terminate.")
+        val configurationFileUrl = getConfigurationFileUrlForVendor()
+
+        if (configurationFileUrl == null) {
+            LOGGER.error("The configuration file for specified vendor $vendor was not found")
+            LOGGER.error("Did you add --vendor argument?")
+            LOGGER.error("The app will terminate")
             exitProcess(-1)
         }
+
+        LOGGER.info("Reading configuration for vendor $vendor from $configurationFileUrl")
+        vendorConfigurationProperties = jacksonObjectMapper().readValue(configurationFileUrl)
+        LOGGER.info("The config is: $vendorConfigurationProperties")
+    }
+
+    private fun getConfigurationFileUrlForVendor(): URL? {
+        return this.javaClass::class.java.getResource("/configuration/$vendor.json")
     }
 }
