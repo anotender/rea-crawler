ebce967df01c71a7472278ecb4782b26ece08067
Mateusz Nowak <mateusznowak@vp.pl>
2019-03-07 20:00:45 +0100

    Replaced vendor argument with spring profile

--- src/main/kotlin/pl/edu/agh/rea/crawler/configuration/provider/ConfigurationProvider.kt
+++ src/main/kotlin/pl/edu/agh/rea/crawler/configuration/provider/ConfigurationProvider.kt
@@ -10,7 +10,7 @@ import java.net.URL
 import kotlin.system.exitProcess
 
 @Component
-class ConfigurationProvider(@Value("\${vendor}") private val vendor: String) {
+class ConfigurationProvider(@Value("\${spring.profiles.active}") private val profile: String) {
 
     companion object {
         private val LOGGER = LoggerFactory.getLogger(ConfigurationProvider::class.java)
@@ -22,19 +22,19 @@ class ConfigurationProvider(@Value("\${vendor}") private val vendor: String) {
         val configurationFileUrl = getConfigurationFileUrlForVendor()
 
         if (configurationFileUrl == null) {
-            LOGGER.error("The configuration file for specified vendor $vendor was not found")
-            LOGGER.error("Did you add --vendor argument?")
+            LOGGER.error("The configuration file for specified profile $profile was not found")
+            LOGGER.error("Did you add --spring.profiles.active argument?")
             LOGGER.error("The app will terminate")
             exitProcess(-1)
         }
 
-        LOGGER.info("Reading configuration for vendor $vendor from $configurationFileUrl")
+        LOGGER.info("Reading configuration for profile $profile from $configurationFileUrl")
         val vendorConfigurationProperties: VendorConfigurationProperties = jacksonObjectMapper().readValue(configurationFileUrl)
         LOGGER.info("The config is: $vendorConfigurationProperties")
         return vendorConfigurationProperties
     }
 
     private fun getConfigurationFileUrlForVendor(): URL? {
-        return this.javaClass::class.java.getResource("/configuration/$vendor.json")
+        return this.javaClass::class.java.getResource("/configuration/$profile.json")
     }
 }
