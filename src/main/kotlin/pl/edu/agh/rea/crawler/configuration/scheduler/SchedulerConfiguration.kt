4ec8ac0bc8e9f0a87bb289b6b6bd045e5fef4eb2
Mateusz Nowak <mateusznowak@vp.pl>
2019-03-24 17:52:16 +0100

    Used thread executor to schedule infinite offer crawling with delay

--- src/main/kotlin/pl/edu/agh/rea/crawler/configuration/scheduler/SchedulerConfiguration.kt
+++ src/main/kotlin/pl/edu/agh/rea/crawler/configuration/scheduler/SchedulerConfiguration.kt
@@ -3,14 +3,28 @@ package pl.edu.agh.rea.crawler.configuration.scheduler
 import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
-import pl.edu.agh.rea.crawler.configuration.scheduler.job.CrawlerJob
+import pl.edu.agh.rea.crawler.configuration.properties.VendorConfiguration
+import pl.edu.agh.rea.crawler.configuration.scheduler.job.OfferCrawlerJob
+import pl.edu.agh.rea.crawler.configuration.scheduler.job.OfferUrlsCrawlerJob
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 
 @Component
-class SchedulerConfiguration(private val crawlerJobs: List<CrawlerJob>) {
+class SchedulerConfiguration(private val offerCrawlerJob: OfferCrawlerJob,
+                             private val offerUrlsCrawlerJob: OfferUrlsCrawlerJob,
+                             private val vendorConfiguration: VendorConfiguration) {
+
+    private val offerCrawlerJobThreadExecutor = Executors.newSingleThreadScheduledExecutor()
 
     @EventListener(ApplicationReadyEvent::class)
     fun scheduleTasks() {
-        crawlerJobs.map { Thread(it) }.forEach { it.start() }
+        Thread(offerUrlsCrawlerJob).start()
+        offerCrawlerJobThreadExecutor.scheduleWithFixedDelay(
+                offerCrawlerJob,
+                vendorConfiguration.requestDelay,
+                vendorConfiguration.requestDelay,
+                TimeUnit.MILLISECONDS
+        )
     }
 
 }
--- src/main/kotlin/pl/edu/agh/rea/crawler/configuration/scheduler/job/OfferCrawlerJob.kt
+++ src/main/kotlin/pl/edu/agh/rea/crawler/configuration/scheduler/job/OfferCrawlerJob.kt
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Component
 import pl.edu.agh.rea.crawler.configuration.kafka.producer.OfferSender
 import pl.edu.agh.rea.crawler.configuration.properties.VendorConfiguration
 import pl.edu.agh.rea.crawler.domain.OfferCrawler
-import java.lang.Thread.sleep
 
 @Component
 class OfferCrawlerJob(private val vendorConfiguration: VendorConfiguration,
@@ -20,15 +19,11 @@ class OfferCrawlerJob(private val vendorConfiguration: VendorConfiguration,
     }
 
     override fun run() {
-        while (true) {
-            val numberOfUrlsToScrap = getNumberOfUrlsToScrap()
-            LOGGER.info("Scraping offers from $numberOfUrlsToScrap urls")
-            for (i in 0 until numberOfUrlsToScrap) {
-                processUrl(urlsToScrap.removeAt(i))
-            }
-            LOGGER.info("Waiting ${vendorConfiguration.requestDelay / 1000} [s]")
-            LOGGER.info("${urlsToScrap.size} remain in queue to scrap")
-            sleep(vendorConfiguration.requestDelay)
+        LOGGER.info("${urlsToScrap.size} remain in queue to scrap")
+        val numberOfUrlsToScrap = getNumberOfUrlsToScrap()
+        LOGGER.info("Scraping offers from $numberOfUrlsToScrap urls")
+        for (i in 0 until numberOfUrlsToScrap) {
+            processUrl(urlsToScrap.removeAt(i))
         }
     }
 
