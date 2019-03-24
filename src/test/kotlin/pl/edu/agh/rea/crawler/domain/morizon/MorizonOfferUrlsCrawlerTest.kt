57f2ec6092cb88a466cd329797026ac9e9d0e606
Mateusz Nowak <mateusznowak@vp.pl>
2019-03-24 20:20:22 +0100

    Extracted common part of OfferUrlsCrawler tests

--- src/test/kotlin/pl/edu/agh/rea/crawler/domain/morizon/MorizonOfferUrlsCrawlerTest.kt
+++ src/test/kotlin/pl/edu/agh/rea/crawler/domain/morizon/MorizonOfferUrlsCrawlerTest.kt
@@ -1,46 +1,13 @@
 package pl.edu.agh.rea.crawler.domain.morizon
 
-import kotlinx.coroutines.runBlocking
-import org.assertj.core.api.BDDAssertions.then
-import org.junit.Test
 import pl.edu.agh.rea.crawler.domain.BaseOfferUrlsCrawlerTest
-import pl.edu.agh.rea.crawler.domain.OfferUrlsCrawler
 
 class MorizonOfferUrlsCrawlerTest : BaseOfferUrlsCrawlerTest("morizon") {
 
-    @Test
-    fun shouldReturnListOfUrlsForGivenPage() {
-        //given
-        val offerUrlsCrawler: OfferUrlsCrawler = getOfferUrlsCrawler()
-        val offerUrlsPageUrl = getResourceUrl("offer_urls_page.html")
-
-        //when
-        val result = runBlocking { offerUrlsCrawler.fetch(offerUrlsPageUrl) }
-
-        //then
-        then(result)
-                .isNotNull
-                .hasSize(3)
-                .containsExactly(
-                        "https://www.morizon.pl/oferta/sprzedaz-mieszkanie-krakow-mistrzejowice-73m2-mzn2032058697",
-                        "https://www.morizon.pl/oferta/sprzedaz-mieszkanie-krakow-grzegorzki-34m2-mzn2032058695",
-                        "https://www.morizon.pl/oferta/sprzedaz-mieszkanie-bytom-szombierki-zabrzanska-65m2-mzn2032057520"
-                )
-    }
-
-    @Test
-    fun shouldReturnEmptyListForPageWithNoOffers() {
-        //given
-        val offerUrlsCrawler: OfferUrlsCrawler = getOfferUrlsCrawler()
-        val offerUrlsPageUrl = getResourceUrl("offer_urls_empty_page.html")
-
-        //when
-        val result = runBlocking { offerUrlsCrawler.fetch(offerUrlsPageUrl) }
-
-        //then
-        then(result)
-                .isNotNull
-                .isEmpty()
-    }
+    override fun getExpectedUrls(): List<String> = listOf(
+            "https://www.morizon.pl/oferta/sprzedaz-mieszkanie-krakow-mistrzejowice-73m2-mzn2032058697",
+            "https://www.morizon.pl/oferta/sprzedaz-mieszkanie-krakow-grzegorzki-34m2-mzn2032058695",
+            "https://www.morizon.pl/oferta/sprzedaz-mieszkanie-bytom-szombierki-zabrzanska-65m2-mzn2032057520"
+    )
 
 }
--- src/test/kotlin/pl/edu/agh/rea/crawler/domain/otodom/OtoDomOfferUrlsCrawlerTest.kt
+++ src/test/kotlin/pl/edu/agh/rea/crawler/domain/otodom/OtoDomOfferUrlsCrawlerTest.kt
@@ -1,47 +1,14 @@
 package pl.edu.agh.rea.crawler.domain.otodom
 
-import kotlinx.coroutines.runBlocking
-import org.assertj.core.api.BDDAssertions.then
-import org.junit.Test
 import pl.edu.agh.rea.crawler.domain.BaseOfferUrlsCrawlerTest
-import pl.edu.agh.rea.crawler.domain.OfferUrlsCrawler
 
 class OtoDomOfferUrlsCrawlerTest : BaseOfferUrlsCrawlerTest("otodom") {
 
-    @Test
-    fun shouldReturnListOfUrlsForGivenPage() {
-        //given
-        val offerUrlsCrawler: OfferUrlsCrawler = getOfferUrlsCrawler()
-        val offerUrlsPageUrl = getResourceUrl("offer_urls_page.html")
-
-        //when
-        val result = runBlocking { offerUrlsCrawler.fetch(offerUrlsPageUrl) }
-
-        //then
-        then(result)
-                .isNotNull
-                .hasSize(4)
-                .containsExactly(
-                        "https://www.otodom.pl/oferta/funkcjonalna-kawalerka-na-blisko-metra-slodowiec-ID3WVxi.html#14fcfe9f8e",
-                        "https://www.otodom.pl/oferta/kawalerka-wysoki-standard-ul-pilotow-eng-ID3WVvo.html#14fcfe9f8e",
-                        "https://www.otodom.pl/oferta/nowe-ladne-2-pok-woronicza-przy-mordorze-i-metrze-ID3WVv4.html#14fcfe9f8e",
-                        "https://www.otodom.pl/oferta/luksusowe-piekne-mieszkanie-z-tarasem-na-mokotowie-ID3GqY8.html#14fcfe9f8e"
-                )
-    }
-
-    @Test
-    fun shouldReturnEmptyListForPageWithNoOffers() {
-        //given
-        val offerUrlsCrawler: OfferUrlsCrawler = getOfferUrlsCrawler()
-        val offerUrlsPageUrl = getResourceUrl("offer_urls_empty_page.html")
-
-        //when
-        val result = runBlocking { offerUrlsCrawler.fetch(offerUrlsPageUrl) }
-
-        //then
-        then(result)
-                .isNotNull
-                .isEmpty()
-    }
+    override fun getExpectedUrls(): List<String> = listOf(
+            "https://www.otodom.pl/oferta/funkcjonalna-kawalerka-na-blisko-metra-slodowiec-ID3WVxi.html#14fcfe9f8e",
+            "https://www.otodom.pl/oferta/kawalerka-wysoki-standard-ul-pilotow-eng-ID3WVvo.html#14fcfe9f8e",
+            "https://www.otodom.pl/oferta/nowe-ladne-2-pok-woronicza-przy-mordorze-i-metrze-ID3WVv4.html#14fcfe9f8e",
+            "https://www.otodom.pl/oferta/luksusowe-piekne-mieszkanie-z-tarasem-na-mokotowie-ID3GqY8.html#14fcfe9f8e"
+    )
 
 }
